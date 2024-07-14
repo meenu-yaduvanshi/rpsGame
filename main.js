db787e13029114b62cc383523712d889d99531b3
meenu yadav <meenuyaduvanshi24@gmail.com>
2024-07-14 22:30:39 +0530

    breakdown for game

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("hello simran")
+
+// Get all element from html file (for example ->buttons, choices)
+// Define two variable first for user choise, second for computer choise
+// Use of math.random for computer choise
+// Make a array in which store-> Rock, Paper, scissors
+// Define a function
+// Conditions  (for example => if userChoise = Scissor and ComCHoise = Rock  => Computer won!!)
+
+
